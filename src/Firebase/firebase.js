3406249cad8c32f1d67be450fa5090100c18e130
SplashAndCode <mateusz.szmorlinski@gmail.com>
2024-06-04 14:10:47 +0200

    Revert "firebase confing moved to .env"
    This reverts commit cd4fad4e3813110af1d5f0d1f103e624507cc014.

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage, ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyCvJIj5qyrWXF1qyCudp-YJhWcL0ODlZLk",
+  authDomain: "fish-blog-abf39.firebaseapp.com",
+  projectId: "fish-blog-abf39",
+  storageBucket: "fish-blog-abf39.appspot.com",
+  messagingSenderId: "282347077685",
+  appId: "1:282347077685:web:de2a3c6e76553054a5717c",
+  measurementId: "G-63RLERW18J"
 };
 
 // Initialize Firebase
